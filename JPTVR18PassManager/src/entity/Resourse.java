e27c9a86241e07c4d71749194caecc88b25290a1
pupil <pupil@ivkhk.ee>
2020-09-04 15:16:14 +0300

    3 raz

---
+++ JPTVR18PassManager/src/entity/Resourse.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author pupil
+ */
+public class Resourse {
+    private String name;
+    private String url;
+    private String login;
+    private String password;
+
+    public Resourse() {
+    }
+
+    public Resourse(String name, String url, String login, String password) {
+        this.name = name;
+        this.url = url;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.url);
+        hash = 37 * hash + Objects.hashCode(this.login);
+        hash = 37 * hash + Objects.hashCode(this.password);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Resourse other = (Resourse) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.url, other.url)) {
+            return false;
+        }
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Resourse{" + "name=" + name + ", url=" + url + ", login=" + login + ", password=" + password + '}';
+    }
+    
+}
--- JPTVR18PassManager/src/jptvr18passmanager/App.java
+++ JPTVR18PassManager/src/jptvr18passmanager/App.java
@@ -5,12 +5,51 @@
  */
 package jptvr18passmanager;
 
+import entity.Resourse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 class App {
+    private List<Resourse> listResourse = new ArrayList<>();
     public void run(){
-            System.out.println("Hello!");
+            System.out.println("--- Менеджер паролей ---");
+            String repeat = "r";
+            do{
+            System.out.println("Выберите задачу");
+            System.out.println("0 - выйти из программы");
+            System.out.println("1 - добавть ресурс");
+            System.out.println("2 - список ресурсов");
+            Scanner scanner = new Scanner(System.in);
+            int task = scanner.nextInt();
+            scanner.nextLine();
+            switch (task) {
+                case 0:
+                    repeat = "q";
+
+                    break;
+                case 1:
+                    System.out.println("Выбрана задача 1");
+                    Resourse resource = new Resourse();
+                    resource.setName("ivkhk");
+                    resource.setUrl("http://kutsehariduskeskus.ee");
+                    resource.setLogin("admin");
+                    resource.setPassword("12345");
+                    listResourse.add(resource);
+                    break;
+                case 2:
+                    System.out.println("Выбрана задача 2");
+                    System.out.println("Список ресурсов:");
+                    for(Resourse r : listResourse){
+                        System.out.println(r.toString());
+                    }
+                    break;
+            }       
+        }while(!"q".equals(repeat));
     }
 }
+
